88f3e8bab30ca990b30e0230f92397fcaa10168b
Voluptua <80710668+Voluptua@users.noreply.github.com>
2023-12-06 13:48:35 +0100

    should work but for real input, takes too long

--- 2023/day-05/src/bin/concept_part1.rs
+++ 2023/day-05/src/bin/concept_part1.rs
@@ -3,11 +3,11 @@
 use std::collections::HashMap;
 
 trait Solve {
-    fn solve(self) -> u32;
+    fn solve(self) -> u128;
 }
 
 impl Solve for &str {
-    fn solve(self) -> u32 {
+    fn solve(self) -> u128 {
         let mut chunks: Vec<&str> = self.split("\n\n").collect();
 
         // seed numbers get removed from `chunks` vector and `seed_nums` vector is the vector containing every seed number
@@ -19,16 +19,16 @@ impl Solve for &str {
         // ---------------------------------------------------
         // ---------------------------------------------------
         // seed-to-soil
-        let mut seed_soil_map: HashMap<u32, u32> = HashMap::new();
+        let mut seed_soil_map: HashMap<u128, u128> = HashMap::new();
         let binding = chunks.remove(0).replace("seed-to-soil map:", "");
         let mut seed_to_soil_num_rows: Vec<&str> = binding.split("\n").collect();
         seed_to_soil_num_rows.remove(0);
 
         for row in seed_to_soil_num_rows {
             let nums: Vec<&str> = row.split(" ").collect();
-            let mut dest_range_start = nums[0].trim().parse::<u32>().unwrap();
-            let mut src_range_start = nums[1].trim().parse::<u32>().unwrap();
-            let range = nums[2].trim().parse::<u32>().unwrap();
+            let mut dest_range_start = nums[0].trim().parse::<u128>().unwrap();
+            let mut src_range_start = nums[1].trim().parse::<u128>().unwrap();
+            let range = nums[2].trim().parse::<u128>().unwrap();
 
             seed_soil_map.insert(src_range_start, dest_range_start);
             for _ in 2..=range {
@@ -39,16 +39,16 @@ impl Solve for &str {
         }
 
         // soil-to-fertilizer
-        let mut soil_fertilizer_map: HashMap<u32, u32> = HashMap::new();
+        let mut soil_fertilizer_map: HashMap<u128, u128> = HashMap::new();
         let binding = chunks.remove(0).replace("soil-to-fertilizer map:", "");
         let mut soil_to_fertilizer_num_rows: Vec<&str> = binding.split("\n").collect();
         soil_to_fertilizer_num_rows.remove(0);
 
         for row in soil_to_fertilizer_num_rows {
             let nums: Vec<&str> = row.split(" ").collect();
-            let mut dest_range_start = nums[0].trim().parse::<u32>().unwrap();
-            let mut src_range_start = nums[1].trim().parse::<u32>().unwrap();
-            let range = nums[2].trim().parse::<u32>().unwrap();
+            let mut dest_range_start = nums[0].trim().parse::<u128>().unwrap();
+            let mut src_range_start = nums[1].trim().parse::<u128>().unwrap();
+            let range = nums[2].trim().parse::<u128>().unwrap();
 
             soil_fertilizer_map.insert(src_range_start, dest_range_start);
             for _ in 2..=range {
@@ -59,16 +59,16 @@ impl Solve for &str {
         }
 
         // fertilizer-to-water
-        let mut fertilizer_water_map: HashMap<u32, u32> = HashMap::new();
+        let mut fertilizer_water_map: HashMap<u128, u128> = HashMap::new();
         let binding = chunks.remove(0).replace("fertilizer-to-water map:", "");
         let mut fertilizer_to_water_num_rows: Vec<&str> = binding.split("\n").collect();
         fertilizer_to_water_num_rows.remove(0);
 
         for row in fertilizer_to_water_num_rows {
             let nums: Vec<&str> = row.split(" ").collect();
-            let mut dest_range_start = nums[0].trim().parse::<u32>().unwrap();
-            let mut src_range_start = nums[1].trim().parse::<u32>().unwrap();
-            let range = nums[2].trim().parse::<u32>().unwrap();
+            let mut dest_range_start = nums[0].trim().parse::<u128>().unwrap();
+            let mut src_range_start = nums[1].trim().parse::<u128>().unwrap();
+            let range = nums[2].trim().parse::<u128>().unwrap();
 
             fertilizer_water_map.insert(src_range_start, dest_range_start);
             for _ in 2..=range {
@@ -79,16 +79,16 @@ impl Solve for &str {
         }
 
         // water-to-light
-        let mut water_light_map: HashMap<u32, u32> = HashMap::new();
+        let mut water_light_map: HashMap<u128, u128> = HashMap::new();
         let binding = chunks.remove(0).replace("water-to-light map:", "");
         let mut water_to_light_num_rows: Vec<&str> = binding.split("\n").collect();
         water_to_light_num_rows.remove(0);
 
         for row in water_to_light_num_rows {
             let nums: Vec<&str> = row.split(" ").collect();
-            let mut dest_range_start = nums[0].trim().parse::<u32>().unwrap();
-            let mut src_range_start = nums[1].trim().parse::<u32>().unwrap();
-            let range = nums[2].trim().parse::<u32>().unwrap();
+            let mut dest_range_start = nums[0].trim().parse::<u128>().unwrap();
+            let mut src_range_start = nums[1].trim().parse::<u128>().unwrap();
+            let range = nums[2].trim().parse::<u128>().unwrap();
 
             water_light_map.insert(src_range_start, dest_range_start);
             for _ in 2..=range {
@@ -99,16 +99,16 @@ impl Solve for &str {
         }
 
         // light-to-temperature
-        let mut light_temperature_map: HashMap<u32, u32> = HashMap::new();
+        let mut light_temperature_map: HashMap<u128, u128> = HashMap::new();
         let binding = chunks.remove(0).replace("light-to-temperature map:", "");
         let mut light_to_temperature_num_rows: Vec<&str> = binding.split("\n").collect();
         light_to_temperature_num_rows.remove(0);
 
         for row in light_to_temperature_num_rows {
             let nums: Vec<&str> = row.split(" ").collect();
-            let mut dest_range_start = nums[0].trim().parse::<u32>().unwrap();
-            let mut src_range_start = nums[1].trim().parse::<u32>().unwrap();
-            let range = nums[2].trim().parse::<u32>().unwrap();
+            let mut dest_range_start = nums[0].trim().parse::<u128>().unwrap();
+            let mut src_range_start = nums[1].trim().parse::<u128>().unwrap();
+            let range = nums[2].trim().parse::<u128>().unwrap();
 
             light_temperature_map.insert(src_range_start, dest_range_start);
             for _ in 2..=range {
@@ -119,16 +119,16 @@ impl Solve for &str {
         }
 
         // temperature-to-humidity
-        let mut temperature_humidity_map: HashMap<u32, u32> = HashMap::new();
+        let mut temperature_humidity_map: HashMap<u128, u128> = HashMap::new();
         let binding = chunks.remove(0).replace("temperature-to-humidity map:", "");
         let mut temperature_to_humidity_num_rows: Vec<&str> = binding.split("\n").collect();
         temperature_to_humidity_num_rows.remove(0);
 
         for row in temperature_to_humidity_num_rows {
             let nums: Vec<&str> = row.split(" ").collect();
-            let mut dest_range_start = nums[0].trim().parse::<u32>().unwrap();
-            let mut src_range_start = nums[1].trim().parse::<u32>().unwrap();
-            let range = nums[2].trim().parse::<u32>().unwrap();
+            let mut dest_range_start = nums[0].trim().parse::<u128>().unwrap();
+            let mut src_range_start = nums[1].trim().parse::<u128>().unwrap();
+            let range = nums[2].trim().parse::<u128>().unwrap();
 
             temperature_humidity_map.insert(src_range_start, dest_range_start);
             for _ in 2..=range {
@@ -139,16 +139,16 @@ impl Solve for &str {
         }
 
         // humidity-to-location
-        let mut humidity_location_map: HashMap<u32, u32> = HashMap::new();
+        let mut humidity_location_map: HashMap<u128, u128> = HashMap::new();
         let binding = chunks.remove(0).replace("humidity-to-location map:", "");
         let mut humidity_to_location_num_rows: Vec<&str> = binding.split("\n").collect();
         humidity_to_location_num_rows.remove(0);
 
         for row in humidity_to_location_num_rows {
             let nums: Vec<&str> = row.split(" ").collect();
-            let mut dest_range_start = nums[0].trim().parse::<u32>().unwrap();
-            let mut src_range_start = nums[1].trim().parse::<u32>().unwrap();
-            let range = nums[2].trim().parse::<u32>().unwrap();
+            let mut dest_range_start = nums[0].trim().parse::<u128>().unwrap();
+            let mut src_range_start = nums[1].trim().parse::<u128>().unwrap();
+            let range = nums[2].trim().parse::<u128>().unwrap();
 
             humidity_location_map.insert(src_range_start, dest_range_start);
             for _ in 2..=range {
@@ -159,12 +159,12 @@ impl Solve for &str {
         }
         // ---------------------------------------------------
         // ---------------------------------------------------
-
-        let mut location_nums: Vec<u32> = vec![];
+        println!("im still doing something! ");
+        let mut location_nums: Vec<u128> = vec![];
 
         // turn into function usages
         for seed_num in seed_nums {
-            let seed_num = seed_num.trim().parse::<u32>().unwrap();
+            let seed_num = seed_num.trim().parse::<u128>().unwrap();
             let mut soil_test = seed_soil_map.get(&seed_num);
             let mut soil_num;
             if soil_test.is_none() {
@@ -197,24 +197,24 @@ impl Solve for &str {
                 light_num = *light_test.unwrap();
             }
 
-            let mut temperature_test = light_temperature_map.get(&light_num);
-            let mut temperature_num;
+            let temperature_test = light_temperature_map.get(&light_num);
+            let temperature_num;
             if temperature_test.is_none() {
                 temperature_num = light_num;
             } else {
                 temperature_num = *temperature_test.unwrap();
             }
 
-            let mut humidity_test = temperature_humidity_map.get(&temperature_num);
-            let mut humidity_num;
+            let humidity_test = temperature_humidity_map.get(&temperature_num);
+            let humidity_num;
             if humidity_test.is_none() {
                 humidity_num = temperature_num;
             } else {
                 humidity_num = *humidity_test.unwrap();
             }
 
-            let mut location_test = humidity_location_map.get(&humidity_num);
-            let mut location_num;
+            let location_test = humidity_location_map.get(&humidity_num);
+            let location_num;
             if location_test.is_none() {
                 location_num = humidity_num;
             } else {
@@ -225,22 +225,31 @@ impl Solve for &str {
         }
 
         let mut max = 0;
+        let mut min = 0; 
+        for loc in location_nums.iter() {
+            if loc > &max {
+                max = *loc;
+            }
+        }
+
         for loc in location_nums {
-            if loc > max {
+            if loc < max {
                 max = loc;
+                min = loc;
             }
         }
 
         //println!("{:#?}", seed_soil_map);
         //println!("{:#?}", seed_nums);
-        max
+        min
     }
 }
 
 fn main() {
     let input = include_str!("../../input.txt");
+    let result = input.solve();
 
-    println!("{}", input.solve());
+    println!("{}", result);
 }
 
 #[cfg(test)]
