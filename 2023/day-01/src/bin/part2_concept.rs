53f93fe4dff9d481f840329f26b1ba7444ba0699
Voluptua <80710668+Voluptua@users.noreply.github.com>
2023-12-06 22:13:13 +0100

    Day-01 part II concept only works for the example

--- 2023/day-01/src/bin/part2_concept.rs
+++ 2023/day-01/src/bin/part2_concept.rs
@@ -1,44 +1,46 @@
+// works for the example but not for the real input
+
 fn replace_spelled_with_digit(line: &str) -> String {
     let mut i = 0;
     let mut line = line.to_owned();
-    
-    loop {
-        println!("At index: {i}");
-        let chunk = &line[i..];
 
+    loop {
+        let chunk = &line[i..];
+        // why x? I think because of array stuff idk
         if chunk.starts_with("one") {
             line.replace_range(i..i+3, "1");
             i += "one".len();
         } else if chunk.starts_with("two") {
-            line.replace_range(i..i+3, "2");
+            line.replace_range(i..i+3, "2xx");
             i += "two".len();
         } else if chunk.starts_with("three") {
-            line.replace_range(i..i+5, "3");
+            line.replace_range(i..i+5, "3xxxx");
             i += "three".len();
         } else if chunk.starts_with("four") {
-            line.replace_range(i..i+4, "4");
+            line.replace_range(i..i+4, "4xxx");
             i += "four".len();
         } else if chunk.starts_with("five") {
-            line.replace_range(i..i+4, "5");
+            line.replace_range(i..i+4, "5xxx");
             i += "five".len();
         } else if chunk.starts_with("six") {
-            line.replace_range(i..i+3, "6");
+            line.replace_range(i..i+3, "6xx");
             i += "six".len();
         } else if chunk.starts_with("seven") {
-            line.replace_range(i..i+5, "7");
+            line.replace_range(i..i+5, "7xxxx");
             i += "seven".len();
         } else if chunk.starts_with("eight") {
-            line.replace_range(i..i+5, "8");
+            line.replace_range(i..i+5, "8xxxx");
             i += "eight".len();
         } else if chunk.starts_with("nine") {
-            line.replace_range(i..i+4, "9");
+            line.replace_range(i..i+4, "9xxx");
             i += "nine".len();
         }
 
         else {
             i += 1;
         }
-        if i > line.len()  {break;}
+        if i >= line.len()  {break;}
+        
     }
     println!("{line}");
     line
